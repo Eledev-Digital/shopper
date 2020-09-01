a5d1598f378273933e7d933cbe0ddb361de7f4a9
James Litchfield <james.litchfield99@gmail.com>
2020-09-01 15:34:15 +0100

    move line items to checkout folder

---
+++ lib/model/checkout/line_item.dart
@@ -0,0 +1,33 @@
+import 'package:shopper/model/orders/discounts.dart';
+import 'package:shopper/model/products/price.dart';
+import 'package:shopper/model/products/variant.dart';
+
+class LineItem {
+  final String id;
+  final int quantity;
+  final String title;
+  final Variant variant;
+  final Price unitPrice;
+
+  final List<DiscountAllocation> discountAlocations;
+
+  LineItem({
+    this.id,
+    this.title,
+    this.variant,
+    this.quantity,
+    this.unitPrice,
+    this.discountAlocations,
+  });
+
+  static LineItem fromJson(Map<String, dynamic> data) {
+    var node = data['node'] ?? {};
+    return LineItem(
+      id: node['id'],
+      quantity: node['quantity'],
+      variant: Variant.fromJson(node['variant'] ?? {}),
+      title: node['title'],
+      unitPrice: Price.fromJson(node['unitPrice'] ?? {}),
+    );
+  }
+}
