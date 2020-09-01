8a696296be660c6ed6ba1c6ce5e71a89abba5d9f
James Litchfield <james.litchfield99@gmail.com>
2020-09-01 15:35:18 +0100

    create order line item model

---
+++ lib/model/orders/order_line_item.dart
@@ -0,0 +1,34 @@
+import 'package:shopper/model/orders/discounts.dart';
+import 'package:shopper/model/products/price.dart';
+import 'package:shopper/model/products/variant.dart';
+
+class OrderLineItem {
+  final String id;
+  final int quantity;
+  final String title;
+  final Variant variant;
+  final Price originalPrice;
+  final Price discountedPrice;
+  final List<DiscountAllocation> discountAlocations;
+
+  OrderLineItem({
+    this.id,
+    this.title,
+    this.variant,
+    this.quantity,
+    this.originalPrice,
+    this.discountedPrice,
+    this.discountAlocations,
+  });
+
+  static OrderLineItem fromJson(Map<String, dynamic> data) {
+    var node = data['node'] ?? {};
+    return OrderLineItem(
+        id: node['id'],
+        quantity: node['quantity'],
+        variant: Variant.fromJson(node['variant'] ?? {}),
+        title: node['title'],
+        discountedPrice: Price.fromJson(node['discountedTotalPrice'] ?? {}),
+        originalPrice: Price.fromJson(node['originalTotalPrice'] ?? {}));
+  }
+}
